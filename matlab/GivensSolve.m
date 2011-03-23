2722c53953812ba370fbd90d55a08db561276c36
John Rogers <jgrogers@gmail.com>
2011-03-23 15:37:20 -0400

    Removed some slow printing commands

--- matlab/GivensSolve.m
+++ matlab/GivensSolve.m
@@ -9,17 +9,15 @@ for j=1:min(m,n)
         a1 = R(j,j);
         a2 = R(i,j);
         [c,s] = cmp_giv(a1,a2);
-        R([j,i],j:n)
+        
         R([j, i],j:n) = [c -s; s c]*R([j,i],j:n);
-        R([j,i],j:n)
+        
     end
 end
 QTb = R(:,n);
 R = R(:,1:n-1);
 
 function [c,s] = cmp_giv(a,b)
-a
-b
 
     if (b == 0)
        c = 1; s = 0;
